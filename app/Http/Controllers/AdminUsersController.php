481eac912ad3cce4b550523609335aace28f2e77
Brent X <harryshum02@gmail.com>
2018-11-01 02:22:27 +0000

    Users section working 100%. Security not installed

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests;
 use App\User;
 use App\Role;
 use App\Photo;
+use App\Http\Requests;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEditRequest;
 
 class AdminUsersController extends Controller
 {
@@ -43,7 +44,6 @@ class AdminUsersController extends Controller
     public function store(UsersRequest $request)
     {
         
-        
         if(trim($request->password) == ''){
             $input = $request->except('password');
         } else{
@@ -60,8 +60,6 @@ class AdminUsersController extends Controller
 
         User::create($input);
         return redirect('admin/users');
-
-
     }
 
     /**
@@ -83,7 +81,9 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        return view('admin.users.edit');
+        $user = User::findOrFail($id);
+        $roles = Role::lists('name', 'id')->all();
+        return view('admin.users.edit', compact('user', 'roles'));
 
     }
 
@@ -94,9 +94,24 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+        if(trim($request->password) == ''){
+            $input = $request->except('password');
+        } else{
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }        
+        
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+            $file->move('image', $name);
+            $photo = Photo::create(['file'=>'$name']);
+            $input['photo_id'] = $photo->id;
+        }
+        $user->update($input);
+        return redirect('admin/users');
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -20,7 +20,8 @@
     <tbody>
         <tr>
           <td>{{$user->id}}</td>
-          <td>{{$user->name}}</td>
+          <td><img height='50' src="{{$user->photo ? $user->photo->file: 'No user photo'}}"></img></td>
+          <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
           <td>{{$user->email}}</td>
           <td>{{$user->role ? $user->role->name : 'User has no role'}}</td>
           <td>{{$user->is_active == 1 ? 'Active' : 'Not Active' }}</td>
